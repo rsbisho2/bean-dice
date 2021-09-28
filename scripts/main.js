01e678f8073fccbe3201f8e4f28bd1e161afe004
Ryan <rsbisho2@ncsu.edu>
2021-09-27 23:03:12 -0400

    MAJOR refactor to add more beans

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,21 @@ Hooks.once('diceSoNiceInit', (dice3d) => {
     dice3d.addTexture("Beans1", {
         name: "Beans1",
         composite: "source-over",
-        source: "modules/bean-dice/beans/beans1.jpg",
+        source: "modules/bean-dice/images/beans1.jpg",
+        bump:""
+    });
+
+    dice3d.addTexture("Beans2", {
+        name: "Beans2",
+        composite: "source-over",
+        source: "modules/bean-dice/images/beans2.png",
+        bump:""
+    });
+
+    dice3d.addTexture("Dank1", {
+        name: "Dank1",
+        composite: "source-over",
+        source: "modules/bean-dice/images/dank1.png",
         bump:""
     });
 });
