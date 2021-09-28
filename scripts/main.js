1dbb8a22173fdc0a46b2aa07cc48d5303024a29b
Ryan <rsbisho2@ncsu.edu>
2021-09-27 22:43:57 -0400

    updated path to image in script

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ Hooks.once('diceSoNiceInit', (dice3d) => {
     dice3d.addTexture("Beans1", {
         name: "Beans1",
         composite: "source-over",
-        source: "beans/beans1.jpg",
+        source: "modules/bean-dice/beans/beans1.jpg",
         bump:""
     });
 });
